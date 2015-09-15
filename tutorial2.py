f4604d9d0b9d3a4e6bc41781e559042e52a529ab
voidJeff <qy120838@126.com>
2015-09-15 13:44:56 -0400

    Updated from Brython Server: 9/15/2015 1:44:55 PM

--- tutorial2.py
+++ tutorial2.py
@@ -9,7 +9,7 @@ black = Color(0x000000, 1.0)
 thinline = LineStyle(0, black)
 mycircle = CircleAsset(5, thinline, blue)
 myother = CircleAsset(5, thinline, red)
-xcoordinates = range(100, 600, 5)
+xcoordinates = range(100, 600, 8)
 
 sprites = [Sprite(mycircle, (x, 15000/x)) for x in xcoordinates]
 sprites = [Sprite(myother, (x, x * 2)) for x in xcoordinates]
